f29f8d2583ef21a442de700b57b23db0381db167
Thiago Lago <thiagolago@asti-dev.presidencia.gov.br>
2019-02-08 10:47:50 -0200

    gerando pdf

--- gestao_rh/apps/funcionarios/urls.py
+++ gestao_rh/apps/funcionarios/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import funcionariosList, funcionariosUpdate, funcionariosDelete, funcionariosNovo
+from .views import ( 
+    funcionariosList, funcionariosUpdate, funcionariosDelete, funcionariosNovo, Pdf #PdfDebug 
+    )
+from .views import relatorio_funcionarios
 
 urlpatterns = [
     path('', funcionariosList.as_view(), name="funcionario_list"),
     path('editar/<int:pk>/', funcionariosUpdate.as_view(), name="update_funcionario"),
     path('delete/<int:pk>/', funcionariosDelete.as_view(), name="delete_funcionario"),
     path('novo/', funcionariosNovo.as_view(), name="create_funcionario"),
+    path('relatorio_funcionarios', relatorio_funcionarios, name='relatorio_funcionarios'),
+    path('relatorio_funcionarios_html', Pdf.as_view(), name='relatorio_funcionarios_html'),
+    #path('relatorio_funcionarios_html_debug', PdfDebug.as_view(), name='relatorio_funcionarios_html_debug'),
 ]
